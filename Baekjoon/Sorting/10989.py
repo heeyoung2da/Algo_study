b26eb13cfd28e99fe60e663a171fe1c164be2b6a
heeyoung2da <mnkl1225@gmail.com>
2022-04-20 20:33:50 +0900

    Add BOJ Problem

---
+++ Baekjoon/Sorting/10989.py
@@ -0,0 +1,12 @@
+# 수 정렬하기 3
+import sys
+
+N = int(input())
+arr = [0] * 10001
+
+for i in range(N) : 
+	arr[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    for j in range(arr[i]):
+        sys.stdout.write(str(i) + "\n")
---
+++ Baekjoon/Sorting/2751.py
@@ -0,0 +1,11 @@
+# 수 정렬하기 2
+
+N = int(input())
+nums = []
+for i in range(N):
+    nums.append(int(input()))
+
+nums.sort()
+
+for i in range(N):
+    print(nums[i])
